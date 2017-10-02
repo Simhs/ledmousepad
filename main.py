cf1cfe0cec688ddce5ae636ce515359290c5f528
Simhs <shs900701@gmail.com>
2017-10-02 21:45:01 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 
 
 
+
 def cropFrom2t1Rate(img):
     h = img.shape[0]
     w =img.shape[1]
@@ -23,13 +24,23 @@ def cropFrom2t1Rate(img):
             crop_w -= 2
             crop_img = img[int((h - crop_h)/2.0): int((h - crop_h)/2.0) + crop_h, int((w - crop_w)/2.0): int((w - crop_w)/2.0) + crop_w]
             return crop_img
+
+cap = cv2.VideoCapture('video.mp4')
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    crop = cropFrom2t1Rate(frame)
+    resized = cv2.resize(crop, (64, 32), interpolation=cv2.INTER_CUBIC)
+    cv2.imshow('frame', resized)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 ori_img = cv2.imread("testdot.PNG")
 h = ori_img.shape[0]
 w = ori_img.shape[1]
 cv2.imshow("hi", ori_img)
 # 비율주어주면 이미지 최대한 중심에서 크게 자르기
 crop = cropFrom2t1Rate(ori_img)
-resized = cv2.resize(crop, (64,32), interpolation=cv2.INTER_CUBIC)
+
 print resized.shape
 cv2.imshow("hi",crop)
 cv2.imshow("hi2",ori_img)
